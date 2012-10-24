ef424f93466f387697fdb32de95256de17564a60
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2012-10-24 18:08:14 +0200

    Add delete button with render form on list view (index)

--- Generator/BootstrapDoctrineCrudGenerator.php
+++ Generator/BootstrapDoctrineCrudGenerator.php
@@ -13,7 +13,6 @@ use Doctrine\ORM\Mapping\ClassMetadataInfo;
  */
 class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
 {
-    
     /**
      * Constructor.
      *
@@ -23,6 +22,63 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
     public function __construct(Filesystem $filesystem, $skeletonDir)
     {
         parent::__construct($filesystem, $skeletonDir);
+    }
+    
+    /**
+     * Generate the CRUD controller.
+     *
+     * @param BundleInterface   $bundle           A bundle object
+     * @param string            $entity           The entity relative class name
+     * @param ClassMetadataInfo $metadata         The entity class metadata
+     * @param string            $format           The configuration format (xml, yaml, annotation)
+     * @param string            $routePrefix      The route name prefix
+     * @param array             $needWriteActions Wether or not to generate write actions
+     *
+     * @throws \RuntimeException
+     */
+    public function generate(BundleInterface $bundle, $entity, ClassMetadataInfo $metadata, $format, $routePrefix, $needWriteActions)
+    {
+        parent::generate($bundle, $entity, $metadata, $format, $routePrefix, $needWriteActions);
+        $dir = sprintf('%s/Resources/views/%s', $this->bundle->getPath(), str_replace('\\', '/', $this->entity));
+        $this->generateDeleteFormView($dir);
+    }
+    
+    /**
+     * Generates the index.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    private function generateIndexView($dir)
+    {
+        $parts = explode('\\', $this->entity);
+        $entityClass = array_pop($parts);
+
+        $this->renderFile($this->skeletonDir, 'views/index.html.twig', $dir.'/index.html.twig', array(
+            'dir'               => $this->skeletonDir,
+            'entity'            => $this->entity,
+            'fields'            => $this->metadata->fieldMappings,
+            'actions'           => $this->actions,
+            'record_actions'    => $this->getRecordActions(),
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'bundle'            => $this->bundle->getName(),
+            'entity_class'      => $entityClass,
+        ));
+    }
+
+    /**
+     * Generates the deleteForm.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function generateDeleteFormView($dir)
+    {
+        $this->renderFile($this->skeletonDir, 'views/deleteForm.html.twig', $dir.'/deleteForm.html.twig', array(
+            'dir'               => $this->skeletonDir,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'entity'            => $this->entity
+        ));
     }
     
     /**
--- Resources/skeleton/crud/bootstrap/actions/delete.php
+++ Resources/skeleton/crud/bootstrap/actions/delete.php
@@ -26,6 +26,38 @@
 
         return $this->redirect($this->generateUrl('{{ route_name_prefix }}'));
     }
+    
+    /**
+     * Display {{ entity }} deleteForm.
+     *
+{% if 'annotation' == format %}
+     * @Template()
+{% endif %}
+     */
+    public function deleteFormAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('{{ bundle }}:{{ entity }}')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find {{ entity }} entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+{% if 'annotation' == format %}
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+{% else %}
+        return $this->render('{{ bundle }}:{{ entity|replace({'\\': '/'}) }}:deleteForm.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+{% endif %}
+    }
 
     private function createDeleteForm($id)
     {
