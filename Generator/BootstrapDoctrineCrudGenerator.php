539256b881053f694a8982fd084266d1b36cd227
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-10-29 19:20:37 +0100

    Add extends based on adminbase.html.twig located in the generated bundle (which include bootstrap) Start add translations

--- Generator/BootstrapDoctrineCrudGenerator.php
+++ Generator/BootstrapDoctrineCrudGenerator.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Symfony\Component\Locale\Locale;
 
 /**
  * Generates a CRUD controller.
@@ -23,7 +24,7 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
     {
         parent::__construct($filesystem, $skeletonDir);
     }
-    
+
     /**
      * Generate the CRUD controller.
      *
@@ -39,10 +40,14 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
     public function generate(BundleInterface $bundle, $entity, ClassMetadataInfo $metadata, $format, $routePrefix, $needWriteActions)
     {
         parent::generate($bundle, $entity, $metadata, $format, $routePrefix, $needWriteActions);
+        $dir = sprintf('%s/Resources/views', $this->bundle->getPath());
+        $this->generateAdminBaseView($dir);
+        $dir = sprintf('%s/Resources/translations', $this->bundle->getPath());
+        $this->copyTranslations($dir);
         $dir = sprintf('%s/Resources/views/%s', $this->bundle->getPath(), str_replace('\\', '/', $this->entity));
         $this->generateDeleteFormView($dir);
     }
-    
+
     /**
      * Generates the index.html.twig template in the final bundle.
      *
@@ -62,7 +67,59 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
             'route_prefix'      => $this->routePrefix,
             'route_name_prefix' => $this->routeNamePrefix,
             'bundle'            => $this->bundle->getName(),
-            'entity_class'      => $entityClass
+            'entity_class'      => $entityClass,
+        ));
+    }
+
+    /**
+     * Generates the show.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function generateShowView($dir)
+    {
+        $this->renderFile($this->skeletonDir, 'views/show.html.twig', $dir.'/show.html.twig', array(
+            'dir'               => $this->skeletonDir,
+            'entity'            => $this->entity,
+            'fields'            => $this->metadata->fieldMappings,
+            'actions'           => $this->actions,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'bundle'            => $this->bundle->getName(),
+        ));
+    }
+
+    /**
+     * Generates the new.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function generateNewView($dir)
+    {
+        $this->renderFile($this->skeletonDir, 'views/new.html.twig', $dir.'/new.html.twig', array(
+            'dir'               => $this->skeletonDir,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'entity'            => $this->entity,
+            'actions'           => $this->actions,
+            'bundle'            => $this->bundle->getName(),
+        ));
+    }
+
+    /**
+     * Generates the edit.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function generateEditView($dir)
+    {
+        $this->renderFile($this->skeletonDir, 'views/edit.html.twig', $dir.'/edit.html.twig', array(
+            'dir'               => $this->skeletonDir,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'entity'            => $this->entity,
+            'actions'           => $this->actions,
+            'bundle'            => $this->bundle->getName(),
         ));
     }
 
@@ -80,7 +137,39 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
             'entity'            => $this->entity
         ));
     }
-    
+
+    /**
+     * Generates the adminbase.html.twig template in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function generateAdminBaseView($dir)
+    {
+        if(!is_file($dir.'/adminbase.html.twig')) {
+            $this->renderFile($this->skeletonDir, 'views/adminbase.html.twig', $dir.'/adminbase.html.twig', array());
+        }
+    }
+
+    /**
+     * Generates the translation files in the final bundle.
+     *
+     * @param string $dir The path to the folder that hosts templates in the bundle
+     */
+    protected function copyTranslations($dir)
+    {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        foreach(Locale::getLanguages() as $lang) {
+            $src = sprintf('%s/translations/messages.%s.yml', $this->skeletonDir, $lang);
+            $dest = sprintf('%s/messages.%s.yml', $dir, $lang);
+            if(is_file($src) && !is_file($dest)) {
+                copy($src, $dest);
+            }
+        }
+    }
+
     /**
      * Returns an array of record actions to generate (edit, show).
      *
@@ -92,5 +181,4 @@ class BootstrapDoctrineCrudGenerator extends DoctrineCrudGenerator
             return in_array($item, array('show', 'edit', 'delete'));
         });
     }
-    
 }
