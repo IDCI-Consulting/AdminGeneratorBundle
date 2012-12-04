02df61ded8cac174a1e5d5010cc9dc9a3b03cf2e
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-12-04 12:00:07 +0100

    Fix coding convention

--- Resources/skeleton/crud/bootstrap/actions/index.php
+++ Resources/skeleton/crud/bootstrap/actions/index.php
@@ -17,10 +17,9 @@
         $pager->setMaxPerPage($this->container->getParameter('max_per_page'));
 
         try {
-            $pager->setCurrentPage($request->query->get('page', 1)); 
-        }
-        catch (NotValidCurrentPageException $e) {
-            throw new NotFoundHttpException(); 
+            $pager->setCurrentPage($request->query->get('page', 1));
+        } catch (NotValidCurrentPageException $e) {
+            throw new NotFoundHttpException();
         }
 
 {% if 'annotation' == format %}
